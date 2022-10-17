bcd98f3748ead5a404f613cd4ace02ca9dd48263
John Andrews <johnandrews.ml@gmail.com>
2022-10-16 21:52:21 -0400

    Filled in character management functions.

--- dosa/dosa.py
+++ dosa/dosa.py
@@ -22,6 +22,7 @@ class Dosa:
 
     dnd = game.Game()
 
+
     @staticmethod
     @client.command(
         name="post",
@@ -62,8 +63,47 @@ class Dosa:
     @staticmethod
     @client.modal("post_modal")
     async def post_modal_response(ctx: CommandContext, name: str, descr: str):
-        result, response = Dosa.dnd.new_quest(name, descr)
+        _, response = Dosa.dnd.new_quest(name, descr, ctx.user.id)
         await ctx.send(response)
+
+
+    @staticmethod
+    @client.command(
+        name="take_down_quest",
+        description="Remove a quest from the bulletin board.",
+        options = [
+            interactions.Option(
+                name="quest_id",
+                description="Quest id of the quest to remove.",
+                type=interactions.OptionType.INTEGER,
+                required=True,
+            )
+        ],
+    )
+    async def take_down_quest_command(ctx: CommandContext, quest_id: int):
+        channel = await ctx.get_channel()
+        if channel.id == toks.bulletin_id:
+            result, response = Dosa.dnd.remove_quest(quest_id, ctx.user.id)
+            if not result:
+                await ctx.send(response, ephemeral=True)
+                return
+            else:
+                history = await channel.get_history()
+                history = [] if not history else history
+                for message in history:
+                    if message.content.startswith(f"#{quest_id}"):
+                        await message.delete()
+                await ctx.send(response, ephemeral=True)
+                return
+        else:
+            guild = await ctx.get_guild()
+            channels = await guild.get_all_channels()
+            bulletin_channel = [ch.name for ch in channels if ch.id == toks.bulletin_id]
+            if len(bulletin_channel) > 0:
+                await ctx.send(f"This command can only be run in {bulletin_channel[0]}", ephemeral=True)
+            else:
+                await ctx.send("No bulletin board set up. Ask your admin do create one!", ephemeral=True)
+        
 
 
     @staticmethod
@@ -74,18 +114,23 @@ class Dosa:
             interactions.Option(
                 name="quest_id",
                 description="The id of the quest you want to join.",
-                type=interactions.OptionType.STRING,
+                type=interactions.OptionType.INTEGER,
                 required=True,
             ),
             interactions.Option(
                 name="character_name",
                 description="The name of character you'd like to join quest as.",
                 type=interactions.OptionType.STRING,
+                required=False,
             ),
         ],
     )
-    async def join_command(ctx: CommandContext, quest_id: str, character_name: str):
-        result, response = Dosa.dnd.join_quest(ctx.user.id if not character_name else character_name, quest_id)
+    async def join_command(ctx: CommandContext, quest_id: str, character_name: str | None = None):
+        if character_name:
+            _, response = Dosa.dnd.join_quest_by_character(quest_id, ctx.user.id, character_name)
+        else:
+            _, response = Dosa.dnd.join_quest_by_id(quest_id, ctx.user.id)
+        
         await ctx.send(response, ephemeral=True)
 
 
@@ -97,13 +142,16 @@ class Dosa:
             interactions.Option(
                 name="quest_id",
                 description="The id of the quest you want to leave.",
-                type=interactions.OptionType.STRING,
+                type=interactions.OptionType.INTEGER,
                 required=True,
             ),
         ],
     )
-    async def leave_command(ctx: CommandContext, quest_name: str):
-        await ctx.send(f"{ctx.author.name} wants to leave quest: {quest_name}! Fill in functionality.")
+    async def leave_command(ctx: CommandContext, quest_id: int):
+        await ctx.send(f"{ctx.author.name} wants to leave quest: {quest_id}! Fill in functionality.")
+
+
+    # Character Management Commands
     
     @staticmethod
     @client.command(
@@ -119,8 +167,9 @@ class Dosa:
         ],
     )
     async def new_character_command(ctx: CommandContext, character_name: str):
-        result, response = Dosa.dnd.new_character(character_name, ctx.user.id)
-        await ctx.send(response)
+        print(ctx.user.id)
+        _, response = Dosa.dnd.new_character(ctx.user.id, character_name)
+        await ctx.send(response, ephemeral=True)
 
     @staticmethod
     @client.command(
@@ -136,4 +185,44 @@ class Dosa:
         ],
     )
     async def switch_character_command(ctx: CommandContext, character_name: str):
-        await ctx.send(f"{ctx.user.username} wants to play as {character_name}.")
+        print(ctx.user.id)
+        result, response = Dosa.dnd.switch_character(ctx.user.id, character_name)
+        if result:
+            try:
+                # change user nickname
+                guild = await ctx.get_guild()
+                await guild.modify_member(ctx.user.id, nick=character_name)
+            except:
+                response += " Cannot change your nickname."
+
+        await ctx.send(response, ephemeral=True)
+
+    @staticmethod
+    @client.command(
+        name="list_characters",
+        description="Get a list of your registered characters."
+    )
+    async def list_characters_command(ctx: CommandContext):
+        print(ctx.user.id)
+        _, response = Dosa.dnd.list_characters(ctx.user.id)
+        await ctx.send(response, ephemeral=True)
+
+    @staticmethod
+    @client.command(
+        name="remove_character",
+        description="Remove a character you have registered.",
+        options= [
+            interactions.Option(
+                name="character_name",
+                description="Name of character you want to remove",
+                type=interactions.OptionType.STRING,
+                required=True,
+            ),
+        ],
+    )
+    async def remove_character_command(ctx: CommandContext, character_name: str):
+        _, response = Dosa.dnd.remove_character(ctx.user.id, character_name)
+        if character_name == ctx.author.nick:
+            # change user nickname
+            pass
+        await ctx.send(response, ephemeral=True)
--- dosa/game.py
+++ dosa/game.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 class Character:
-    def __init__(self, character_name: str, user_id: str):
+    def __init__(self, character_name: str, user_id: int):
         self.character_name = character_name
         self.user_id = user_id
 
@@ -9,14 +9,18 @@ class Character:
         return self.__hash__() == other.__hash__()
 
     def __hash__(self):
-        return self.user_id.__hash__()
+        return self.user_id.__hash__() ^ self.character_name.__hash__()
 
 
 class Quest:
-    def __init__(self, name: str, description: str, id: int):
+    def __init__(self, name: str, description: str, id: int, user_id: int):
         self.name = name
         self.description = description
         self.quest_id = id
+        self.user_id = user_id
+
+    def can_edit(self, user_id: int):
+        return self.user_id == user_id
 
     def __eq__(self, other):
         return self.__hash__() == other.__hash__()
@@ -25,95 +29,137 @@ class Quest:
         return self.quest_id.__hash__()
 
     def __str__(self):
-        return f"__**Quest:**__ *{self.name}* #{self.quest_id}\n\n `{self.description}`"
+        return f"#{self.quest_id} __**Quest:**__ *{self.name}*\n\n `{self.description}`"
 
 
 class BulletinBoard:
     def __init__(self):
-        self.quests_and_players = defaultdict(lambda : set())
-        self.all_quests = defaultdict(lambda : None)
+        self.quests_and_characters = defaultdict(lambda : defaultdict(lambda : Character("Default", 0)))
+        self.all_quests = defaultdict(lambda : Quest("Default", "Default", 0, 0))
 
     def add_quest(self, new_quest: Quest):
         self.all_quests[new_quest.quest_id] = new_quest # type: ignore
+        return True, str(new_quest)
 
-    def remove_quest(self, quest: Quest):
-        if quest not in self.quests_and_players:
+    def remove_quest(self, quest_id: int, user_id: int):
+        print(f"{quest_id} by {user_id}")
+        for k,v in self.all_quests.items():
+            print(k)
+        if quest_id not in self.all_quests:
             return False, "Error: Quest not found."
+
+        quest = self.all_quests[quest_id]
+        if not quest.can_edit(user_id):
+            return False, "You cannot takedown other's quests."
         else:
-            del self.quests_and_players[quest]
-            del self.all_quests[quest.quest_id]
-            return  True, f"Deleted quest *{quest.name}*"
+            if quest_id in self.quests_and_characters:
+                del self.quests_and_characters[quest_id]
+            del self.all_quests[quest_id]
+            return  True, f"Deleted quest *{quest.name}*."
 
     def enlist_to_quest(self, quest_id: int, character: Character):
         if quest_id not in self.all_quests:
             return False, "Error: Quest not found."
-        quest: Quest = self.all_quests[quest_id] # type: ignore
 
-        if character in self.quests_and_players[quest]:
+        quest: Quest = self.all_quests[quest_id] # type: ignore
+        if character.user_id in self.quests_and_characters[quest_id]:
             return False, "You are already enlisted to this quest."
 
-        self.quests_and_players[quest].add(character)
+        self.quests_and_characters[quest_id][character.user_id] = character
         return True, f"*{character.character_name}* has joined *{quest.name}*!"
 
     def leave_quest(self, quest_id: int, user_id: int):
         if quest_id not in self.all_quests:
-            return False, "Error: Quest not found"
+            return False, "Error: Quest not found."
         quest: Quest = self.all_quests[quest_id] # type: ignore
-
-        if user_id not in self.quests_and_players[quest]:
+        if user_id not in self.quests_and_characters[quest_id]:
             return False, f"You are not enlisted in *{quest.name}"
         else:
-            character_name = self.quests_and_players[quest].intersection({user_id}).pop().character_name
-            self.quests_and_players[quest].remove(user_id)
+            character_name = self.quests_and_characters[quest_id][user_id].character_name
+            del self.quests_and_characters[quest_id][user_id]
             return True, f"You have left *{quest.name}* as character {character_name}"
 
-    def get_quests_players(self, quest: Quest):
-        return self.quests_and_players[quest]
+    def get_quests_players(self, quest_id: int):
+        return [c.character_name for c in self.quests_and_characters[quest_id].values()]
 
 
 class Game:
-
-    current_quest_id = 0
 
     def __init__(self):
         self.bulletin_board = BulletinBoard()
         self.all_characters = defaultdict(lambda : set())
         self.current_characters = defaultdict(lambda : None)
+        self.current_quest_id = 0
 
-    @staticmethod
-    def inc_quest_id():
-        Game.current_quest_id += 1
-        return Game.current_quest_id
+    # Quest Management
+    def inc_quest_id(self):
+        self.current_quest_id += 1
+        return self.current_quest_id
 
-    def new_quest(self, quest_name, quest_description):
-        new_quest = Quest(quest_name, quest_description, Game.inc_quest_id())
-        self.bulletin_board.add_quest(new_quest)
-        return True, str(new_quest)
+    def new_quest(self, quest_name, quest_description, user_id):
+        new_quest = Quest(quest_name, quest_description, self.inc_quest_id(), user_id)
+        return self.bulletin_board.add_quest(new_quest)
 
-    def join_quest(self, user_id, quest_id):
+    def remove_quest(self, quest_id, user_id):
+        return self.bulletin_board.remove_quest(quest_id, user_id)
+
+    def join_quest_by_id(self, quest_id, user_id):
         character = self.current_characters[user_id]
         if not character:
-            return False, "Error: Please register a character."
+            return False, "Error: Not currently playing as a character, switch to one of your registered characters."
 
         return self.bulletin_board.enlist_to_quest(quest_id, character)
+
+    def join_quest_by_character(self, quest_id, user_id, character_name):
+        characters = self.all_characters[user_id]
+        for character in characters:
+            if character.character_name == character_name:
+                return self.bulletin_board.enlist_to_quest(quest_id, character)
+
+        return False, f"Character {character_name} not found in your registered characters."
 
     def leave_quest(self, user_id, quest_id):
         return self.bulletin_board.leave_quest(quest_id, user_id)
 
-
-    def get_current_character(self, user_id):
-        return self.all_characters[user_id]
-
-    def new_character(self, character_name, user_id):
-        if character_name in self.all_characters[character_name]:
-            return False, "You alread have a character with this name."
+    ## Character Management
+    def new_character(self, user_id, character_name):
+        for character in self.all_characters[user_id]:
+            if character_name == character.character_name:
+                return False, "You alread have a character with this name."
 
         new_character = Character(character_name, user_id)
         self.all_characters[user_id].add(new_character)
         return True, f"The world welcome {character_name}!\n To join quests with this character use `/switch_character {character_name}`."
 
+    def remove_character(self, user_id, character_name):
+        for character in self.all_characters[user_id]:
+            if character_name == character.character_name:
+                self.all_characters[user_id].remove(character)
+                if self.current_characters[user_id].character_name == character.character_name: # type: ignore
+                    del self.current_characters[user_id]
+                return True, f"*{character.character_name}* has been removed from your registered characters."
+
+        return False, f"You do not have a character named *{character_name}*."
+
     def switch_character(self, user_id, character_name):
-        if character_name not in self.all_characters[user_id]:
-            return False, f"You do not have a character named *{character_name}*."
-        else:
-            return True, f"You are now playing as *{character_name}*."
+        for character in self.all_characters[user_id]:
+            if character_name == character.character_name:
+                self.current_characters[user_id] = character
+                return True, f"You are now playing as *{character_name}*."
+
+        return False, f"You do not have a character named *{character_name}*."
+
+    def list_characters(self, user_id):
+        response = "Your characters:\n`"
+        user_characters = self.all_characters[user_id]
+        
+        if not len(user_characters) > 0:
+            response += "No registered characters.`\n"
+            return True, response
+
+        for character in user_characters:
+            response += f"  * {character.character_name}\n"
+        response += "`"
+        return True, response
+
+    
